2270700a1194d8dfa42472cc5da9b6142c8fb664
apetrovskiy <apetrovskiy@hotmail.com>
2014-07-23 01:27:12 +0400

    .

---
+++ workspace-sts-3.6.0.RELEASE/HelloWorld/src/main/resources/com/apress/prospring3/ch2/HelloWorldMessageProvider.java
@@ -0,0 +1,9 @@
+package com.apress.prospring3.ch2;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Hello World!";
+	}
+}
