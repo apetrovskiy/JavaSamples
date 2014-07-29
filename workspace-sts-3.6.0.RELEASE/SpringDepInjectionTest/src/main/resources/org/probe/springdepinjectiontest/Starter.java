e3dca09da7eac2548b4a6061c8b9e0abace8fb62
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2014-07-29 11:12:52 +0400

    Spring in action, up to 2.2.4

--- workspace-sts-3.6.0.RELEASE/SpringDepInjectionTest/src/main/resources/org/probe/springdepinjectiontest/Starter.java
+++ workspace-sts-3.6.0.RELEASE/SpringDepInjectionTest/src/main/resources/org/probe/springdepinjectiontest/Starter.java
@@ -17,6 +17,9 @@ public class Starter {
 		
 		performer = (Performer)ctx.getBean("kenny2");
 		performer.perform();
+		
+		performer = (Performer)ctx.getBean("hank");
+		performer.perform();
 	}
 
 }
