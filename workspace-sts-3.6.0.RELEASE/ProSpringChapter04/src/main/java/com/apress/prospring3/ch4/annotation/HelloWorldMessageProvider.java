cebf04604f258d28a586370b55aa56cb3b75f175
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2014-08-14 13:33:29 +0400

    .

--- workspace-sts-3.6.0.RELEASE/ProSpringChapter04/src/main/java/com/apress/prospring3/ch4/annotation/HelloWorldMessageProvider.java
+++ workspace-sts-3.6.0.RELEASE/ProSpringChapter04/src/main/java/com/apress/prospring3/ch4/annotation/HelloWorldMessageProvider.java
@@ -3,6 +3,8 @@ package com.apress.prospring3.ch4.annotation;
 import com.apress.prospring3.ch4.MessageProvider;
 import org.springframework.stereotype.Service;
 
+/*
+// this works
 @Service("messageProvider")
 public class HelloWorldMessageProvider implements MessageProvider {
 
@@ -12,3 +14,4 @@ public class HelloWorldMessageProvider implements MessageProvider {
 	}
 
 }
+*/
