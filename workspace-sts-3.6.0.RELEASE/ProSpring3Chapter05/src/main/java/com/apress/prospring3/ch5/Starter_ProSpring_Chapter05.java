b249c45236dfc378c2708c715b3fc6176a15e315
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2014-08-25 13:02:03 +0400

    Pro Spring 3, up to 5.9

--- workspace-sts-3.6.0.RELEASE/ProSpring3Chapter05/src/main/java/com/apress/prospring3/ch5/Starter_ProSpring_Chapter05.java
+++ workspace-sts-3.6.0.RELEASE/ProSpring3Chapter05/src/main/java/com/apress/prospring3/ch5/Starter_ProSpring_Chapter05.java
@@ -2,8 +2,11 @@ package com.apress.prospring3.ch5;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import com.apress.prospring3.ch5.lifecycle.DestructiveBean;
+import com.apress.prospring3.ch5.lifecycle.DestructiveBeanWithInterface;
 import com.apress.prospring3.ch5.lifecycle.SimpleBean;
 import com.apress.prospring3.ch5.lifecycle.SimpleBeanWithInterface;
+import com.apress.prospring3.ch5.lifecycle.SimpleBeanWithJsr250;
 
 public class Starter_ProSpring_Chapter05 {
 
@@ -18,13 +21,41 @@ public class Starter_ProSpring_Chapter05 {
 		// this works
 		// SimpleBean simpleBean3 = SimpleBean.getBean("simpleBean3", ctx001);
 		
+		GenericXmlApplicationContext ctx002 = new GenericXmlApplicationContext();
+		ctx002.load("classpath:META-INF/spring/lifecycle/initMethod.xml");
+		ctx002.refresh();
+		SimpleBeanWithInterface simpleBean01 = SimpleBeanWithInterface.getBean("simpleBean01", ctx002);
+		SimpleBeanWithInterface simpleBean02 = SimpleBeanWithInterface.getBean("simpleBean02", ctx002);
+		// this works
+		// SimpleBeanWithInterface simpleBean03 = SimpleBeanWithInterface.getBean("simpleBean03", ctx003);
+		/*
+		// this does not work
 		GenericXmlApplicationContext ctx003 = new GenericXmlApplicationContext();
 		ctx003.load("classpath:META-INF/spring/lifecycle/initMethod.xml");
 		ctx003.refresh();
-		SimpleBeanWithInterface simpleBean01 = SimpleBeanWithInterface.getBean("simpleBean01", ctx003);
-		SimpleBeanWithInterface simpleBean02 = SimpleBeanWithInterface.getBean("simpleBean02", ctx003);
+		SimpleBeanWithJsr250 simpleBean001 = SimpleBeanWithJsr250.getBean("simpleBean001", ctx003);
+		SimpleBeanWithJsr250 simpleBean002 = SimpleBeanWithJsr250.getBean("simpleBean002", ctx003);
 		// this works
-		// SimpleBeanWithInterface simpleBean03 = SimpleBeanWithInterface.getBean("simpleBean03", ctx003);
+		SimpleBeanWithJsr250 simpleBean003 = SimpleBeanWithJsr250.getBean("simpleBean003", ctx003);
+		*/
+		
+		GenericXmlApplicationContext ctx005 = new GenericXmlApplicationContext();
+		// ctx005.load("classpath:lifecycle/disposeMethod.xml");
+		ctx005.load("classpath:META-INF/spring/lifecycle/disposeMethod.xml");
+		ctx005.refresh();
+		DestructiveBean bean005 = (DestructiveBean)ctx005.getBean("destructiveBean005");
+		System.out.println("Calling destroy()");
+		ctx005.destroy();
+		System.out.println("Called destroy()");
+		
+		GenericXmlApplicationContext ctx006 = new GenericXmlApplicationContext();
+		// ctx006.load("classpath:lifecycle/disposeInterface.xml");
+		ctx006.load("classpath:META-INF/spring/lifecycle/disposeInterface.xml");
+		ctx006.refresh();
+		DestructiveBeanWithInterface bean006 = (DestructiveBeanWithInterface)ctx006.getBean("destructiveBean006");
+		System.out.println("Calling destroy()");
+		ctx006.destroy();
+		System.out.println("Called destroy()");
 		
 		System.out.println("that's all!");
 	}
