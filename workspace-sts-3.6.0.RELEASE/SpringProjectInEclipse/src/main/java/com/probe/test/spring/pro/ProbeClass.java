4e415882f8b3d8f0b797a2272d31ce73e1d372f6
apetrovskiy <apetrovskiy@hotmail.com>
2014-08-27 22:21:32 +0400

    .

--- workspace-sts-3.6.0.RELEASE/SpringProjectInEclipse/src/main/java/com/probe/test/spring/pro/ProbeClass.java
+++ workspace-sts-3.6.0.RELEASE/SpringProjectInEclipse/src/main/java/com/probe/test/spring/pro/ProbeClass.java
@@ -2,7 +2,7 @@ package com.probe.test.spring.pro;
 
 public class ProbeClass {
 	private String field;
-	public void setString(String field) {
+	public void setField(String field) {
 		this.field = field;
 	}
 	public String getField() {
