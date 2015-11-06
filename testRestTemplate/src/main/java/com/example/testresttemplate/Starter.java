7b3521953094a4e959b226852119eb7d7ec85b2d
apetrovskiy <apetrovskiy@hotmail.com>
2015-11-06 23:41:38 +0300

    experiments with IDEA 15

---
+++ testRestTemplate/src/main/java/com/example/testresttemplate/Starter.java
@@ -0,0 +1,10 @@
+package com.example.testresttemplate;
+
+/**
+ * Created by alexa_000 on 10/09/2015.
+ */
+public class Starter {
+    public static void main(String[] args) {
+        RestRequestCreator creator = new RestRequestCreator();
+    }
+}
