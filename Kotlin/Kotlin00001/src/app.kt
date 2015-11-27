ac9c5f63dd41f03bdc58351dd70c2cc385ad0eb3
apetrovskiy <apetrovskiy@hotmail.com>
2015-11-27 23:52:55 +0300

    probe

---
+++ Kotlin/Kotlin00001/src/app.kt
@@ -0,0 +1,6 @@
+/**
+ * Created by alexa_000 on 12/11/2015.
+ */
+fun main(args: Array<String>) {
+    println("!!!")
+}
---
+++ Kotlin/Kotlin00002/src/Invoice.kt
@@ -0,0 +1,5 @@
+/**
+ * Created by alexa_000 on 12/11/2015.
+ */
+class Invoice {
+}
