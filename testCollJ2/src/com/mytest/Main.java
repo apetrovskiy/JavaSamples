a22ee69dce17deb4f33491a20d6532f1d4e63a91
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2015-11-28 03:11:06 +0300

    Naughton, Shildt, J2

---
+++ testCollJ2/src/com/mytest/Main.java
@@ -0,0 +1,14 @@
+package com.mytest;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayListDemo demo01 = new ArrayListDemo();
+        demo01.Test();
+        ArrayListToArray demo02 = new ArrayListToArray();
+        demo02.Test();
+        LinkedListDemo demo03 = new LinkedListDemo();
+        demo03.Test();
+    }
+}
