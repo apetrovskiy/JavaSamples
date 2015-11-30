a48a6631342353a7f475ff0224726debe72e797a
apetrovskiy <apetrovskiy@hotmail.com>
2015-11-30 23:01:40 +0300

    experiments with the Spark framework

--- testFileSystemTools/src/com/mytest/Main.java
+++ testFileSystemTools/src/com/mytest/Main.java
@@ -1,9 +1,6 @@
 package com.mytest;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -18,14 +15,19 @@ public class Main {
         // FileInputStream fin = new FileInputStream("/D/Manuals/EWS1054SDU/245680umRU.pdf");
         // FileInputStream fin = new FileInputStream("E:\\HW\\20151120\\IMG_20151120_150536.jpg");
         Path path = FileSystems.getDefault().getPath("E:/HW/20151120/IMG_20151120_150536.jpg");
-        FileInputStream fin = new FileInputStream("E:/HW/20151120/IMG_20151120_150536.jpg");
-        /*
-        while (fin.available()) {
-            fin.read();
-        }
-        */
-        // fin.close();
+        try (InputStream fin = new FileInputStream("E:/HW/20151120/IMG_20151120_150536.jpg");) {
+            int whatHaveBeenRead = fin.read();
+            System.out.println("have been read at first: " + whatHaveBeenRead);
+            System.out.println("available: " + fin.available());
 
+            int bytes;
+            while ((bytes = fin.read()) != -1) System.out.println("reading further ... " + fin.available());
+
+            fin.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
         File f1 = new File("/");
         File f2 = new File("/", "autoexec.bat" ) ;
         File fЗ = new File(f1, "autoexec.bat") ;
