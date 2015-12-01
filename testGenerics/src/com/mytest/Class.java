c503ae49d0299cf2c9062ad7e4dd0b5642b788ed
Alexander Petrovskiy <apetrovskiy>
2015-12-01 12:31:42 +0300

    experiments with generics

---
+++ testGenerics/src/com/mytest/Class.java
@@ -0,0 +1,14 @@
+package com.mytest;
+
+/**
+ * Created by shuran on 12/1/2015.
+ */
+public class Class<T> {
+    T _val;
+    public Class() {
+    }
+
+    public Class(T _val) {
+        this._val = _val;
+    }
+}
