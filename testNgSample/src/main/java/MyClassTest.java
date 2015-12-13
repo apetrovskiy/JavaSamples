35ecbcd9ca02411dcd710efc0979ec341a17fb49
Alexander Petrovskiy <apetrovskiy>
2015-12-13 22:07:34 +0300

    .

---
+++ testNgSample/src/main/java/MyClassTest.java
@@ -0,0 +1,18 @@
+import static org.testng.Assert.*;
+
+/**
+ * Created by shuran on 12/13/2015.
+ */
+public class MyClassTest {
+/*
+    @org.testng.annotations.BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+    */
+}
