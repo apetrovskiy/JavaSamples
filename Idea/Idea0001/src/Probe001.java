60564ca1f73bcfe6e875b1f2c7d2f862634a524f
Alexander Petrovskiy <apetrovskiy>
2015-12-13 20:54:38 +0300

    ...

---
+++ Idea/Idea0001/src/Probe001.java
@@ -0,0 +1,7 @@
+import com.intellij.ide.ui.laf.intellij.IntelliJCheckBoxUI;
+
+/**
+ * Created by alexa_000 on 03/12/2015.
+ */
+public class Probe001 extends IntelliJCheckBoxUI {
+}
---
+++ testJunitInCommunityEdition/src/com/mytest/MyClass.java
@@ -0,0 +1,7 @@
+package com.mytest;
+
+/**
+ * Created by alexa_000 on 03/12/2015.
+ */
+public class MyClass {
+}
