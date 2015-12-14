1f290c7231f187139b579b09df7668b88422e840
apetrovskiy <apetrovskiy@hotmail.com>
2015-12-15 00:12:58 +0300

    .

--- testJunitInCommunityEdition/src/com/mytest/MyClass.java
+++
@@ -1,7 +0,0 @@
-package com.mytest;
-
-/**
- * Created by alexa_000 on 03/12/2015.
- */
-public class MyClass {
-}
---
+++ testJunitInCommunityEdition/src/com/mytest/RestApiLoader.java
@@ -0,0 +1,7 @@
+package com.mytest;
+
+/**
+ * Created by alexa_000 on 09/12/2015.
+ */
+public class RestApiLoader {
+}
