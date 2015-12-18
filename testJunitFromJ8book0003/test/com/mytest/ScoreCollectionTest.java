ce5f2f5d19a91f463c7a3155094b9f675af93f07
apetrovskiy <apetrovskiy@hotmail.com>
2015-12-18 12:48:56 +0300

    the JUnit in Java 8 book

--- testJunitFromJ8book0003/test/com/mytest/ScoreCollectionTest.java
+++ testJunitFromJ8book0003/test/com/mytest/ScoreCollectionTest.java
@@ -4,7 +4,6 @@ import org.junit.*;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
-import static org.junit.Assert.*;
 
 /**
  * Created by alexa_000 on 17/12/2015.
--- testJunitFromJ8book0005/test/testjunitfromj8book0005/ScoreCollectionTest.java
+++ testJunitFromJ8book0005/test/testjunitfromj8book0005/ScoreCollectionTest.java
@@ -5,13 +5,23 @@
  */
 package testjunitfromj8book0005;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ScoreCollectionTest {
     @Test
-    public void testArithmeticMean() {
-        // fail();
+    public void answerArithmeticMeanOfTwoNumbers() {
+        // Arrange
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+        
+        // Act
+        int actualResult = collection.arithmeticMean();
+        
+        // Assert
+        assertThat(actualResult, CoreMatchers.equalTo(6));
     }
     
 }
