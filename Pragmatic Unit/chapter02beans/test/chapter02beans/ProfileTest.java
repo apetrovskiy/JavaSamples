84d822852e98d94c322f0eed08f8fe4e4ecd5c40
Alexander Petrovskiy <apetrovskiy>
2015-12-23 02:43:59 +0300

    JUnit in Java 8 chapter 02

---
+++ Pragmatic Unit/chapter02beans/test/chapter02beans/ProfileTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter02beans;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author shuran
+ */
+public class ProfileTest {
+    
+    public ProfileTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
