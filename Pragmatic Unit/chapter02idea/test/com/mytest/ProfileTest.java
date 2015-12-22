02b0766321754bfc07491d3712478fa9dd3c085f
Alexander Petrovskiy <apetrovskiy>
2015-12-23 02:15:47 +0300

    JUnit in Java 8 chapter 02

--- Pragmatic Unit/chapter02idea/test/com/mytest/ProfileTest.java
+++ Pragmatic Unit/chapter02idea/test/com/mytest/ProfileTest.java
@@ -1,5 +1,6 @@
 package com.mytest;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,14 +9,21 @@ import static org.junit.Assert.*;
  * Created by shuran on 12/23/2015.
  */
 public class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @Before
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
 
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() throws Exception {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
         criteria.add(criterion);
@@ -25,12 +33,9 @@ public class ProfileTest {
     }
 
     @Test
-    public void matchAnswersTrueForAnyFontCareCriteria() {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got milk?");
+    public void matchAnswersTrueForAnyDontCareCriteria() {
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
         criteria.add(criterion);
